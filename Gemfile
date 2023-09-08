d19f663247fa132bb9ecbddd280dc7ad5dc55f89
Davis Weimer <davisweimer@gmail.com>
2023-09-08 13:27:37 -0600

    webmock & vcr config

--- Gemfile
+++ Gemfile
@@ -58,6 +58,7 @@ group :development, :test do
   gem 'capybara'
   gem 'launchy'
   gem 'simplecov'
+  
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -70,3 +70,11 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+# VCR configuration
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.default_cassette_options = { record: :new_episodes }
+  config.configure_rspec_metadata!
+end
