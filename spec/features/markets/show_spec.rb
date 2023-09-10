e4265ede6f0b0ae5ddbd480c9ed25c089df2c9dc
Davis Weimer <davisweimer@gmail.com>
2023-09-10 13:05:04 -0600

    test: Market Show

--- spec/features/markets/show_spec.rb
+++ spec/features/markets/show_spec.rb
@@ -5,16 +5,23 @@ RSpec.describe "Market", type: :feature do
     @markets_json = MarketsService.all_markets
     @markets = MarketsFacade.get_all_markets
     @market = @markets.last
+    @market_address = MarketsFacade.readable_address(@market.id)
     @vendors = MarketsFacade.get_all_vendors_for_market(@market.id)
   end
 
   describe "Show Page" do
     it "market's name and readable address", :vcr do
       visit market_path(@market.id)
-      save_and_open_page
-      # expect(page).to have_css(".market-name", text: "14&U Farmers' Market")
-      # expect(page).to have_css(".market-address", text: "1400 U Street NW , Washington, District of Columbia 20009")
 
+      expect(page).to have_css(".market-name", text: @market.name)
+      expect(page).to have_css(".market-address", text: @market_address)
+
+      within ".vendor-list" do
+        @vendors.each do |vendor|
+          expect(page).to have_css(".vendor-list-item", text: vendor.name)
+          expect(page).to have_link(vendor.name, href: vendor_path(vendor.id))
+        end
+      end
     end
   end
 end
